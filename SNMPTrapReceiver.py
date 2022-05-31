98370c5b1f671d64e6290906e4e49c8c372090ba
Ruben Vazquez <Ruben.Vazquez@commscope.com>
2022-05-31 01:48:27 -0500

    Collecting traps ok

--- SNMPTrapReceiver.py
+++ SNMPTrapReceiver.py
@@ -2,20 +2,33 @@
 from pysnmp.entity import engine, config
 from pysnmp.carrier.asyncore.dgram import udp
 from pysnmp.entity.rfc3413 import ntfrcv
+from pysnmp.smi import builder, view, compiler, rfc1902
 import logging
+
+# Assemble MIB browser
+mibBuilder = builder.MibBuilder()
+mibViewController = view.MibViewController(mibBuilder)
+compiler.addMibCompiler(
+    mibBuilder, sources=['file:///usr/BNC-MIB.py'])
+
+# Pre-load MIB modules that define objects we receive in TRAPs
+mibBuilder.loadModules('BNC-MIB')
+
 
 snmpEngine = engine.SnmpEngine()
 
-TrapAgentAddress = '10.45.77.164'  # Trap listerner address
-Port = 162  # trap listerner port
+# Trap listerner address
+TrapAgentAddress = '10.45.77.164'
+Port = 162
+
 
 logging.basicConfig(filename='received_traps.log', filemode='w',
                     format='%(asctime)s - %(message)s', level=logging.INFO)
-
 logging.info("Agent is listening SNMP Trap on " +
              TrapAgentAddress+" , Port : " + str(Port))
 logging.info(
     '--------------------------------------------------------------------------')
+
 
 print("Agent is listening SNMP Trap on " +
       TrapAgentAddress+" , Port : " + str(Port))
@@ -32,8 +45,16 @@ config.addV1System(snmpEngine, 'my-area', 'public')
 
 def cbFun(snmpEngine, stateReference, contextEngineId, contextName,
           varBinds, cbCtx):
-    print("Received new Trap message")
-    logging.info("Received new Trap message")
+    print("--------Received new Trap message--------")
+    logging.info("--------Received new Trap message--------")
+
+    varBinds = [
+        rfc1902.ObjectType(rfc1902.ObjectIdentity(
+            x[0]), x[1]).resolveWithMib(mibViewController)
+        for x in varBinds
+    ]
+
+    # Print
     for name, val in varBinds:
         logging.info('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
         print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
